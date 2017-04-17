c54cd2d02cb940cc75ce537c80d8405821fd52cf
Riley <Riley>
2017-04-16 21:25:32 -0700

    Init commit with basic folder layout

---
+++ Assets/_Editor/CreateProjectTree.cs
@@ -0,0 +1,80 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Simplicity
+{
+    public class CreateProjectTree
+    {
+        [MenuItem("Simpicity/Project/Generate Project Tree")]
+        public static void Excute()
+        {
+            var assets = GenerateFolderStructure();
+            CreateFolders(assets);
+        }
+
+        private static void CreateFolders(Folder rootFolder)
+        {
+            if (!AssetDatabase.IsValidFolder(rootFolder.DirPath))
+            {
+                Debug.Log("Creating: <b>" + rootFolder.DirPath + "</b>");
+                AssetDatabase.CreateFolder(rootFolder.ParentPath, rootFolder.name);
+                File.Create(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + rootFolder.DirPath + Path.DirectorySeparatorChar + ".keep");
+            }
+            else
+            {
+                if (Directory.GetFiles(Directory.GetCurrentDirectory() + Path.AltDirectorySeparatorChar + rootFolder.DirPath).Length < 1)
+                {
+                    File.Create(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + rootFolder.DirPath + Path.DirectorySeparatorChar + ".keep");
+                    Debug.Log("Creating '.keep' file in: <b>" + rootFolder.DirPath + "</b>");
+                }
+                else
+                    Debug.Log("Directory <b>" + rootFolder.DirPath + "</b> already exists");
+            }
+
+            rootFolder.subFolders.ForEach(folder => CreateFolders(folder));
+        }
+
+        private static Folder GenerateFolderStructure()
+        {
+            Folder rootFolder = new Folder("Assets", "");
+
+            rootFolder.Add("_Extenions");
+            rootFolder.Add("_Plugins");
+            rootFolder.Add("_3rdParty");
+
+            var GameAssets = rootFolder.Add("_GAME_ASSETS");
+
+            GameAssets.Add("_Data");
+
+            var Scripts = GameAssets.Add("_Scripts");
+            Scripts.Add("_Attributes");
+            Scripts.Add("_Enums");
+            Scripts.Add("_Utility");
+            Scripts.Add("_GameLogic");
+            Scripts.Add("_Structs");
+            Scripts.Add("_Interfaces");
+            Scripts.Add("_ScriptableObjects");
+
+            var Scene = GameAssets.Add("_Scenes");
+            Scene.Add("_Dev");
+            Scene.Add("_Final");
+
+            GameAssets.Add("_Prefab");
+
+            var Resources = GameAssets.Add("_Art");
+            Resources.Add("_Models");
+            Resources.Add("_Animations");
+            Resources.Add("_Animators");
+            Resources.Add("_Effects");
+            Resources.Add("_Materials");
+            Resources.Add("_Shaders");
+            Resources.Add("_Audio");
+            Resources.Add("_Textures");
+            Resources.Add("_Sprites");
+            Resources.Add("_Fonts");
+
+            return rootFolder;
+        }
+    }
+}
---
+++ Assets/_Editor/Folder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Simplicity
+{
+    public class Folder
+    {
+        public string DirPath { get; private set; }
+        public string ParentPath { get; private set; }
+        public string name;
+        public List<Folder> subFolders;
+
+        public Folder Add(string folderName)
+        {
+            Folder subFolder = null;
+            if (ParentPath.Length > 0)
+                subFolder = new Folder(folderName, ParentPath + Path.DirectorySeparatorChar + name);
+            else
+                subFolder = new Folder(folderName, name);
+
+            subFolders.Add(subFolder);
+            return subFolder;
+        }
+
+        public Folder(string name, string dirPath)
+        {
+            this.name = name;
+            ParentPath = dirPath;
+            DirPath = ParentPath + Path.DirectorySeparatorChar + this.name;
+            subFolders = new List<Folder>();
+        }
+    }
+}
